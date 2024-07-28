9e2f8479642e6139e40ce1335d5d7e07e06ff5ac
Dibacarter <165795752+Dibacarter@users.noreply.github.com>
2024-07-29 00:50:11 +0200

    Update ff.js

--- ff.js
+++ ff.js
@@ -1,10 +1,12 @@
-        function openModal(element) {
-            document.getElementById('myModal').style.display = "block";
-            document.body.style.overflow = "hidden"; // Prevent scrolling
-            document.getElementById('modalImage').src = element.src;
-        }
+    function openModal(element) {
+        document.getElementById('myModal').style.display = "block";
+        document.body.style.overflow = "hidden"; // Prevent scrolling
+        document.body.style.paddingRight = "15px"; // Adjust for scrollbar width if necessary
+        document.getElementById('modalImage').src = element.src;
+    }
 
-        function closeModal() {
-            document.getElementById('myModal').style.display = "none";
-            document.body.style.overflow = "auto"; // Allow scrolling again
-        }
+    function closeModal() {
+        document.getElementById('myModal').style.display = "none";
+        document.body.style.overflow = "auto"; // Allow scrolling again
+        document.body.style.paddingRight = "0"; // Reset padding
+    }
