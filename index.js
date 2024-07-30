45f285f76e9ff0e33eef69e5543d641b37a41b6f
Dibacarter <165795752+Dibacarter@users.noreply.github.com>
2024-07-31 01:13:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,20 @@
-        function openModal(element) {
-            document.getElementById('myModal').style.display = "block";
-            document.getElementById('modalImage').src = element.src;
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
 
-        }
-        function closeModal() {
-            document.getElementById('myModal').style.display = "none";
-            document.body.style.overflow = 'auto';
-        }
+function openModal(element) {
+    var modal = document.getElementById('myModal');
+    var modalImage = document.getElementById('modalImage');
+    modal.style.display = "block";
+    modalImage.src = element.src;
+    modalImage.style = element.getAttribute('data-modal-style');
+    document.body.style.overflow = 'hidden';
+    document.body.style.touchAction = 'none';
+    document.querySelector('.gallery').style.overflowY = 'hidden';
+    document.querySelector('.gallery').style.touchAction = 'none';
+}
+
+function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+    document.body.style.overflow = 'auto';
+    document.body.style.touchAction = '';
+    document.querySelector('.gallery').style.overflowY = 'auto';
+    document.querySelector('.gallery').style.touchAction = '';
+}
