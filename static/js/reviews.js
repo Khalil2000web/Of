e86142098d469e67fa329c8e297699592f03e6e6
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-04-13 14:28:55 +0300

    Update reviews.js

--- static/js/reviews.js
+++ static/js/reviews.js
@@ -1,1 +1,18 @@
-function openModal(title, overview, review, info, copyright, element) {const imgSrc = element.querySelector(".movie-poster").src;document.getElementById("modalTitle").innerText = title;document.getElementById("modalReview").innerText = "My opinion: " + review;document.getElementById("modalOverview").innerText = "Overview: " + overview;document.getElementById("modalRights").innerText = copyright;document.getElementById("modalFinfo").innerText = info;document.getElementById("modalImg").src = imgSrc;document.getElementById("movieModal").style.display = "flex";} function closeModal() {document.getElementById("movieModal").style.display = "none";}
+  function openModal(title, overview, review, videoUrl, info, copyright, element) {
+    const imgSrc = element.querySelector(".movie-poster").src;
+
+    document.getElementById("modalTitle").innerText = title;
+    document.getElementById("modalOverview").innerText = "Overview: " + overview;
+    document.getElementById("modalReview").innerText = "My opinion: " + review;
+    document.getElementById("modalFinfo").innerText = info;
+    document.getElementById("modalRights").innerText = copyright;
+    document.getElementById("modalImg").src = imgSrc;
+    document.getElementById("ifr").src = videoUrl;
+
+    document.getElementById("movieModal").style.display = "flex";
+  }
+
+  function closeModal() {
+    document.getElementById("movieModal").style.display = "none";
+    document.getElementById("ifr").src = "";
+  }
